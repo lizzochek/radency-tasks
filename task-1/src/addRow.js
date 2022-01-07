e71c627b969a368cdf63b03886a7d96c0f81865b
lizzochek <littled685@gmail.com>
2022-01-07 20:19:12 +0200

    Adding a task implemented

---
+++ task-1/src/addRow.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//Buttons in rows code
+const buttonInRow =
+  '<button class="btn archivebtn"> <i class="fas fa-archive"></i> </button> <button class="btn deletebtn"><i class="far fa-trash-alt"></i></button>';
+
+const getDate = () => {
+  let today = new Date();
+  const month = today.toLocaleString("default", { month: "long" });
+  console.log(month);
+  return `${today.getDate()} ${month}, ${today.getFullYear()}`;
+};
+
+const getDatesFromString = (str) => {
+  let datesArr = str.match(/\d{2}([\/.-])\d{2}\1\d{4}/g);
+  if (!datesArr) return [];
+
+  let resArr = datesArr.map((el) => el.replace(/-/g, "/"));
+  return resArr.join(", ");
+};
+
+export const addRow = (table, input) => {
+  let newRow = table.insertRow(-1);
+  let datesFromContent = getDatesFromString(input[2].value);
+
+  newRow.insertCell(0).innerHTML = input[0].value;
+  newRow.insertCell(1).innerHTML = getDate();
+  newRow.insertCell(2).innerHTML = input[1].value;
+  newRow.insertCell(3).innerHTML = input[2].value;
+  newRow.insertCell(4).innerHTML = datesFromContent;
+
+  newRow.insertCell(5).insertAdjacentHTML("afterbegin", buttonInRow);
+};
--- task-1/src/index.js
+++ task-1/src/index.js
@@ -1,16 +1,41 @@
 "use strict";
 
+//Helpers
+import { addRow } from "./addRow.js";
+
+//Tables
+
+const activeTable = document.querySelector(".tableActive");
+const archivedTable = document.querySelector(".tableArchived");
+const countTable = document.querySelector(".tableCount");
+
+//Buttons under first table
 const createNote = document.querySelector(".createNote");
 const seeArchived = document.querySelector(".seeArchived");
 
+//Buttons on tasks
 const archiveNote = document.querySelectorAll(".archivebtn");
-const deleteNoteNote = document.querySelectorAll(".deletebtn");
+const deleteNote = document.querySelectorAll(".deletebtn");
 
+//Modal window
 const taskDescription = document.querySelector(".taskDescription");
+const taskDescriptionForm = document.querySelector(".taskDescriptionForm");
+const input = document.querySelectorAll("input");
+const addTaskBtn = document.querySelector(".addTask");
 const overlay = document.querySelector(".overlay");
 
+//Event listeners
 createNote.addEventListener("click", (e) => {
   e.preventDefault();
   taskDescription.classList.remove("hidden");
   overlay.classList.remove("hidden");
+});
+
+addTaskBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  addRow(activeTable, input);
+
+  taskDescriptionForm.reset();
+  taskDescription.classList.add("hidden");
+  overlay.classList.add("hidden");
 });
